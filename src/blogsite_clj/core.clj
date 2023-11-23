684434572ee3c54fd40724575f52988bbdf2e10d
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2023-11-23 22:21:19 +0530

    wrap params middleware

--- src/blogsite_clj/core.clj
+++ src/blogsite_clj/core.clj
@@ -6,6 +6,7 @@
    [compojure.route :refer [not-found]]
    [next.jdbc :as jdbc]
    [ring.adapter.jetty :refer [run-jetty]]
+   [ring.middleware.params :refer [wrap-params]]
    [ring.middleware.refresh :refer [wrap-refresh]]
    [ring.util.response :refer [header]]
    [selmer.parser :refer [cache-off! render-file]]
@@ -14,25 +15,27 @@
 (def db (jdbc/get-datasource {:dbtype "sqlite" :dbname "blog.db"}))
 
 (defn env [key]
-  (read-string (System/getenv key)))
+  (some-> (System/getenv key)
+          (read-string)))
 
 (defn keys-to-keywords [m]
   (into {} (map (fn [[k v]] (vector (keyword k) v)) m)))
 
 (def handler
-  (routes
-   (GET "/blogs" [] (render-file "views/home.html" {:blogs (get-blogs db)}))
-   (GET "/new", [] (render-file "views/new.html" {}))
-   (POST "/blogs" req (let [params (:form-params req)
-                            slug (sluj (get params "title"))
-                            new-blog (-> (select-keys params ["title" "description" "contents"])
-                                         (keys-to-keywords)
-                                         (assoc :slug slug))
-                            url (str "/blogs/" slug)]
-                        (save-blog db new-blog)
-                        (header {} "HX-Location" url)))
-   (GET "/blogs/:slug" [slug] (render-file "views/blog.html" {:blog (get-blog db slug)}))
-   (not-found "Not found")))
+  (wrap-params
+   (routes
+    (GET "/blogs" [] (render-file "views/home.html" {:blogs (get-blogs db)}))
+    (GET "/new", [] (render-file "views/new.html" {}))
+    (POST "/blogs" req (let [params (:form-params req)
+                             slug (sluj (get params "title"))
+                             new-blog (-> (select-keys params ["title" "description" "contents"])
+                                          (keys-to-keywords)
+                                          (assoc :slug slug))
+                             url (str "/blogs/" slug)]
+                         (prn (save-blog db new-blog))
+                         (header {} "HX-Location" url)))
+    (GET "/blogs/:slug" [slug] (render-file "views/blog.html" {:blog (get-blog db slug)}))
+    (not-found "Not found"))))
 
 (when (env "DEVELOPMENT")
   (cache-off!)
